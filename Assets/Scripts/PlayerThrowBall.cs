91d96f1fdc38d20a77b175bbf85999daf72ef3d3
Mayntu <daniilkhusnullin@gmail.com>
2023-06-10 14:26:23 +0600

    ловление мяча

--- Assets/Scripts/PlayerThrowBall.cs
+++ Assets/Scripts/PlayerThrowBall.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using Mirror;
 
-public class ThrowObject : MonoBehaviour
+public class PlayerThrowBall : NetworkBehaviour
 {
-    [SerializeField] float maxPower;
-    [SerializeField] float maxDistance;
-    [SerializeField] float throwAngle;
+    [SerializeField] private float maxPower;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private float throwAngle;
+    [SerializeField] private float powerIncreaseSpeed;
     [SerializeField] private GameObject player;
 
     private bool isThrowing = false;
@@ -12,8 +14,8 @@ public class ThrowObject : MonoBehaviour
     private float currentDistance = 0f;
 
 
-
-    void Update()
+    
+    private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -22,7 +24,7 @@ public class ThrowObject : MonoBehaviour
             currentDistance = 0f;
         }
 
-        if (Input.GetMouseButton(0) && isThrowing)
+        if (Input.GetMouseButton(0) && isThrowing && player != null)
         {
             currentPower += Time.deltaTime * maxPower;
             currentPower = Mathf.Clamp(currentPower, 0f, maxPower);
@@ -32,12 +34,17 @@ public class ThrowObject : MonoBehaviour
             transform.rotation = throwRotation * Quaternion.Euler(throwAngle, 0f, 0f);
         }
 
-        if (Input.GetMouseButtonUp(0) && isThrowing)
+        if (Input.GetMouseButtonUp(0) && isThrowing && player != null)
         {
             isThrowing = false;
             Vector3 throwDirection = transform.forward;
             Vector3 throwForce = throwDirection * currentPower;
             GetComponent<Rigidbody>().AddForce(throwForce, ForceMode.Impulse);
+            player.GetComponent<PlayerCatchBall>().IsCatched = false;
         }
+    }
+    public void AssignPlayer(GameObject player_)
+    {
+        player = player_;
     }
 }
